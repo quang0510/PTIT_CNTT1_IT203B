cb91ebfc6e3070a572a172a2a8b4bde7ada10f51
quang0510 <nguyendangquang0510@gmail.com>
2026-03-13 01:31:27 +0700

    Add files via upload

--- Session_04/KiemTra/InvalidProductException.java
+++ Session_04/KiemTra/InvalidProductException.java
@@ -1,5 +1,6 @@
 package Session_04.KiemTra;
-
 public class InvalidProductException extends RuntimeException {
-
+    public InvalidProductException(String message) {
+        super(message);
+    }
 }
